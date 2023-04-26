94a8cfae58e040c03b37adb4422434332d5a3be1
Richy <richymbiamou52@gmail.com>
2023-04-26 02:25:05 +0100

    mise en place du html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        primary: ['rgb(59, 58, 59)'],
+        secondary: ['rgb(81, 85, 249)'],
+        tertiaire: ['rgb(85, 83, 85)']
+      },
+      fontFamily: {
+        sans: ["'Space Grotesk', sans-serif"]
+      },
+      borderColor: {
+        secondary: ['rgb(81, 85, 249)']
+      },
+      textColor: {
+        secondary: ['rgb(81, 85, 249)']
+      }
+ 
+    },
   },
   plugins: [],
 }
