43a1e0c5eef6ab34980d6da2ab3867bed02b3c4a
Richy <richymbiamou52@gmail.com>
2023-04-26 02:25:23 +0100

    mise en place du javascript

---
+++ script.js
@@ -0,0 +1,45 @@
+const main = document.getElementById('main')
+
+document.body.addEventListener('keydown', (e) =>{
+    const mainEl = `
+    <h1>${e.keyCode}</h1>
+        <div class="card-group">
+            <div class="card">
+                <div class="div1">
+                    <p class="text-white text-center">event.key</p>
+                </div>
+                <div class="div2">
+                    <p>${e.key}</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="div1">
+                    <p>event.location</p>
+                    <a href="#" class="p-left">(?)</a>
+                </div>
+                <div class="div2">
+                    <p>${e.location}</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="div1">
+                    <p>event.which</p>
+                    <a href="#" class="p-left">(?)</a>
+                </div>
+                <div class="div2">
+                    <p>${e.which}</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="div1">
+                    <p>event.code</p>
+                </div>
+                <div class="div2">
+                    <p>${e.code}</p>
+                </div>
+            </div>
+           
+        </div>
+    `
+    main.innerHTML = mainEl
+})
